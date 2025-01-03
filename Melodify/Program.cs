f521e0d794449cd64688facc282375deefbcd62b
Mayara Pereira <mayaracapereira45@gmail.com>
2025-01-02 21:04:09 -0300

    atualizações

--- Melodify/Program.cs
+++ Melodify/Program.cs
@@ -1,14 +1,16 @@
 ﻿// Melodify
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-List<string> listasDasBandas = new List<string> { "U2", "The Beatles", "calipson" };
+//List<string> listasDasBandas = new List<string> { "U2", "The Beatles", "calipson" };
 
-
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 08, 6 });
+bandasRegistradas.Add("Beatles", new List<int>());
 void ExibirLogo()
 {
 
 
     Console.WriteLine(@" 
-MayaraCaroline
+
 
 
 ███╗░░░███╗███████╗██╗░░░░░░█████╗░██████╗░██╗███████╗██╗░░░██╗
@@ -43,7 +45,7 @@ void ExibirOpcoesDoMenu()
             MostrarBandasRegistradas();
             break;
         case 3:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            AvaliarBanda();
             break;
         case 4:
             Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
@@ -56,39 +58,79 @@ void ExibirOpcoesDoMenu()
             break;
     }
 }
+//registra todas as bandas
 void RegistrarBandas()
 {
 
     Console.Clear();
-    Console.WriteLine("******************");
-    Console.WriteLine("Registro de Bandas");
-    Console.WriteLine("******************\n");
+    ExibirTituloDaOpcao("Registro de Bandas");
     Console.Write("Digite o Nome da banda que deseja registrar : ");
 
     string nomeDaBanda = Console.ReadLine()!;
 
-    listasDasBandas.Add(nomeDaBanda);
-    Console.WriteLine($"A banda{nomeDaBanda}foi registrada com Sucesso");
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeDaBanda}foi registrada com Sucesso");
     Thread.Sleep(2000);
     Console.Clear();
     ExibirOpcoesDoMenu();
 }
-
+//Exibe todas as bandas Registradas
 void MostrarBandasRegistradas()
 {
     Console.Clear();
-    Console.WriteLine("**************************");
-    Console.WriteLine("Exibindo todas as Bandas");
-    Console.WriteLine("**************************\n");
-
-    for (int i = 0; i < listasDasBandas.Count; i++)
+    ExibirTituloDaOpcao("Exibindo todas as Bandas");
+    // for (int i = 0; i < listasDasBandas.Count; i++)
+    //{
+    //    Console.WriteLine($"Bandas: {listasDasBandas[i]}");
+    //}
+    foreach (string banda in bandasRegistradas.Keys)
     {
-        Console.WriteLine($"Bandas: {listasDasBandas[i]}");
+        Console.WriteLine($"Bandas: {banda}");
     }
     Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
     Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
 }
+// Conta a quantidade de Letras e acrescenta na mesma quantidade um caracter 
+void ExibirTituloDaOpcao(string titulo)
+{
+    int qntdLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(qntdLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+void AvaliarBanda()
+{
+    //Digitar qual banda  deseja avaliar
+    //Se a banda existir no dicionário >> atribui uma nota
+    // senão, volta para o menor principal
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual nota a {nomeDaBanda} merece: ");
+        int notaBanda = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(notaBanda);
+        Console.WriteLine($"\nA nota foi  registrada com sucesso para a banda {nomeDaBanda}");
+        Thread.Sleep(3000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA Banda {nomeDaBanda}  não foi encontrada");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+
 ExibirOpcoesDoMenu();
 ExibirLogo();
